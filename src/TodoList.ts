f36c1eb426a23e50b2565d3a4abb388d25a17050
paulolopestech <phlj.junior@gmail.com>
2024-02-29 22:10:34 -0300

    :sparkles: feat: add tasks to ToDoList

---
+++ src/TodoList.ts
@@ -0,0 +1,16 @@
+export type Task = {
+    title: string,
+    description: string,
+    targetDate: string,
+    type?: string,
+    priority?: string,
+    subTasks?: Task []
+}
+
+export class ToDoList {
+    private tasks: Task[] = []
+
+    add(task: Task) {
+        this.tasks.push(task)
+    }
+}
