16fc5646067dcd7132b1c13bcc56b67d03f5a082
paulolopestech <phlj.junior@gmail.com>
2024-03-07 21:15:44 -0300

    :sparkles: feat: validate task before insert

--- src/TodoList.ts
+++ src/TodoList.ts
@@ -11,7 +11,17 @@ export class ToDoList {
   private tasks: Task[] = []
 
   add (task: Task) {
-    this.tasks.push(task)
+    const missingProperties = ['title', 'description', 'targetDate'].filter(
+      (prop) => !Object.keys(task).includes(prop)
+    )
+    try {
+      if (missingProperties.length > 0) {
+        return 'Missing properties in task object'
+      }
+      this.tasks.push(task)
+    } catch (error) {
+      return error
+    }
   }
 
   getTasks () {
